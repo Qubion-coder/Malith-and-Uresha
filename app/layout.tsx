c25c5247937ff10d3c14b9d6a95e1d25150c11e1
Sanjaya Perera <sanjayaperera80@gmail.com>
2026-09-16 02:45:47 +0530

    Update metadata for link preview to Malith and Uresha

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: 'VIMUKTHI & PIUMI Wedding',
-  description: 'Join us as we celebrate the blessed Christian union of VIMUKTHI and PIUMI on May 30, 2026.',
+  title: 'Malith & Uresha Wedding',
+  description: 'Join us as we celebrate the wedding of Malith & Uresha.',
   generator: 'v0.app',
   icons: {
     icon: '/wedding-icon.png',
